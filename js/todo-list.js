e7545e9b2b1d6ebd28562ef0425bdcf96dd3d596
Daniel Morais <damorais@gmail.com>
2025-04-06 09:34:29 -0300

    Implementação de filtros

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,5 +1,11 @@
 const todos = [];
 
+const TodoFilter = {
+    ALL: "all",
+    DONE: "done",
+    PENDING: "pending",
+};
+
 document.getElementById("new-todo").addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
         const newTodoInput = document.getElementById("new-todo");
@@ -12,12 +18,32 @@ document.getElementById("new-todo").addEventListener("keypress", function (e) {
     }
 });
 
-function renderTodos() {
+document.getElementById("filter-all").addEventListener("click", function () {
+    renderTodos(TodoFilter.ALL);
+});
+
+document.getElementById("filter-done").addEventListener("click", function () {
+
+    renderTodos(TodoFilter.DONE);
+});
+
+document.getElementById("filter-pending").addEventListener("click", function () {
+    renderTodos(TodoFilter.PENDING);
+});
+
+
+function renderTodos(filter = TodoFilter.ALL) {
     const todoListUl = document.getElementById("todo-list");
 
     todoListUl.innerHTML = "";
 
-    for (const todo of todos) {
+    const filteredTodos = todos.filter((todo) => {
+        if (filter === TodoFilter.ALL) return true;
+        if (filter === TodoFilter.DONE) return todo.done;
+        if (filter === TodoFilter.PENDING) return !todo.done;
+    });
+
+    for (const todo of filteredTodos) {
         const todoItemLi = document.createElement("li");
         todoItemLi.textContent = todo.text;
 
@@ -54,4 +80,6 @@ function markTodoAsDone(todoId) {
     todo.done = true;
 }
 
-renderTodos();
+document.addEventListener("DOMContentLoaded", function () {
+    renderTodos();
+});
